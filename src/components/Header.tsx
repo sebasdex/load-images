4c4ed3068d4ce9af6593edc637181fd4f87d61ab
sebasdex <sebasdc9@hotmail.com>
2025-03-28 11:41:58 -0600

    feat: Update Header component to reflect PicsVibe App branding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 function Header() {
     return (
-        <div className="px-6 py-4 bg-gradient-to-r from-gray-50 to-gray-100">
-            <h1 className="text-3xl md:text-4xl font-extrabold text-gray-800 text-center tracking-tight">
-                Load Images App
+        <header className="px-6 py-3 bg-teal-600">
+            <h1 className="text-3xl md:text-4xl font-bold text-white text-center tracking-tight">
+                PicsVibe App
             </h1>
-        </div>
+        </header>
     )
 }
 
