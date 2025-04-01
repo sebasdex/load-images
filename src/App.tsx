8d5eb0e092b37cd22a096449a68cab912bb9c8fd
sebasdex <sebasdc9@hotmail.com>
2025-04-01 17:38:11 -0600

    feat: Implement dark mode toggle functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDark(!isDark);
+  };
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Header toggleDarkMode={toggleDarkMode} isDark={isDark} />
+      <Main isDark={isDark} />
+      <Footer isDark={isDark} />
     </>
   )
 }
