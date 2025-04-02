f541b19f4df2ffe3dadaa86314e8c82d191da050
sebasdex <sebasdc9@hotmail.com>
2025-04-02 11:16:14 -0600

    feat: Enhance dark mode functionality with local storage and media query support

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode !== null
+      ? JSON.parse(savedMode)
+      : window.matchMedia("(prefers-color-scheme: dark)").matches;
+  });
+
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = (e: MediaQueryListEvent) => {
+      const newMode = e.matches;
+      setIsDark(newMode);
+      localStorage.setItem("darkMode", JSON.stringify(newMode));
+    };
+
+    darkModeMediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      darkModeMediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
 
   const toggleDarkMode = () => {
-    setIsDark(!isDark);
+    setIsDark((prevMode: boolean) => {
+      const newMode = !prevMode;
+      localStorage.setItem("darkMode", JSON.stringify(newMode));
+      return newMode;
+    });
   };
 
   return (
